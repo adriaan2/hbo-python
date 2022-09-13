c2e53046c950a9e84a5f805c9ef372381147c46c
adriaan2 <50466901+adriaan2@users.noreply.github.com>
2022-09-14 00:54:28 +0200

    Add files via upload

---
+++ immediatesuccesor.py
@@ -0,0 +1,11 @@
+from datetime import datetime, timedelta
+
+year=int(input("year"))
+month=int(input("month"))
+day=int(input("day"))
+
+date=datetime(year,month,day,    )
+print(date)
+#for i in range(5): 
+ #   date += datetime.timedelta(day=1)
+   #print(date) 
---
+++ templategenerator.py
@@ -0,0 +1,22 @@
+
+
+check=True
+def accepted():
+ firstname=input("firstname ")
+ cfirstname=firstname.capitalize()
+ lastname=input("firstname ")
+ jobtitle=input("firstname ")
+ salary=input("salary ")
+ print(firstname)
+
+while check==True:
+   awnser=input("would you like more  to writeletters yes/no ")
+   if awnser==("yes"):
+         print("test")
+   
+   
+   elif awnser==("no"):
+         check=False
+   else:
+          print("yes or no please")
+ 
