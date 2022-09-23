3fe22a6462211409f819c96de9a7d0f7cf7e8d66
adriaan2 <50466901+adriaan2@users.noreply.github.com>
2022-09-23 11:54:23 +0200

    Add files via upload

---
+++ templategenerator2.py
@@ -0,0 +1,18 @@
+
+check1=1
+while check1==1:
+   awnser=input("yes or no ")
+   if awnser=="yes":
+      check1=check1+1
+      while check1==2:
+         awnser2=input("Joboffer or rejected")
+         if awnser2=="Joboffer":
+            check1=check1+1
+ 
+      while  check1==3:
+         fname=input("firstname")      
+      while check1==2:
+         print("test")
+   else: 
+      check1=check1-1
+     
